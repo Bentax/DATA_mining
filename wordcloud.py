7b327477dcb0d532c083bcd757d6a04b10d2340c
Andrey Kuzin <bentaxrus@mail.ru>
2024-10-02 16:10:42 +0300

    Create wordcloud.py

---
+++ wordcloud.py
@@ -0,0 +1,24 @@
+# Импорт необходимых библиотек
+import pandas as pd
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# Чтение .csv файла (замените на путь к вашему файлу)
+file_path = 'Стартапы.csv'
+df = pd.read_csv(file_path)
+
+# Обработка текста: выбор столбца "Описание" и объединение всех текстов в одну строку
+text = " ".join(description for description in df["Описание"].dropna())
+
+# Определение стоп-слов
+stopwords = {'а', 'в', 'на', 'с', 'или', 'и', 'по', 'для', 'от', 'из', 'не', 'это', 'мы', 'о', 'что', 'за', 'можно', 'как', 'который', 'до', 'and', 'the', 'to', 'with', 'for'}
+
+# Создание облака слов
+wordcloud = WordCloud(stopwords=stopwords, background_color='white', width=800, height=400).generate(text)
+
+# Визуализация облака слов
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.title("Облако слов для столбца 'Описание'")
+plt.show()
